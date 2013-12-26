fa5579862b6b9a46789c68a88cb273dd35945bd9
jeroenmathon <deathslycer@gmail.com>
2013-12-26 14:21:46 +0100

    Project Init

---
+++ My Little Alchemy Lab/gen/com/example/My_Little_Alchemy_Lab/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.example.My_Little_Alchemy_Lab;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
---
+++ My Little Alchemy Lab/src/com/example/My_Little_Alchemy_Lab/MyActivity.java
@@ -0,0 +1,15 @@
+package com.example.My_Little_Alchemy_Lab;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

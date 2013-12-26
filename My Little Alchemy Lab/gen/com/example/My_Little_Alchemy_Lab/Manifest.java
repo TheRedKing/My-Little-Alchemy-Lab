95aa05e556fad03a60794dbe5fd9942919bef2e6
jeroenmathon <deathslycer@gmail.com>
2013-12-26 14:27:21 +0100

    Project Init

--- My Little Alchemy Lab/gen/com/example/My_Little_Alchemy_Lab/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package com.example.My_Little_Alchemy_Lab;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
